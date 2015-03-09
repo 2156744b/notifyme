10a880162856e518eba141bc845f79e1d003c7b9
2156744b <2156744b@student.gla.ac.uk>
2015-03-09 16:03:28 +0000

    Fixed the navigation drawer

--- src/uk/gla/mobilehci/notifyme/MainActivity.java
+++ src/uk/gla/mobilehci/notifyme/MainActivity.java
@@ -1,16 +1,24 @@
 package uk.gla.mobilehci.notifyme;
 
 import uk.gla.mobilehci.notifyme.fragments.AllEventsFragment;
+import uk.gla.mobilehci.notifyme.fragments.EditAddFriends;
+import uk.gla.mobilehci.notifyme.fragments.FriendEvents;
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+	public static final int SETTINGS_RESULT = 1;
 
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
@@ -27,49 +35,121 @@ public class MainActivity extends Activity {
 		mTitle = mDrawerTitle = getTitle();
 		drawerOptions = getResources().getStringArray(R.array.drawer_options);
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
 		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
 				R.layout.drawer_list_item, drawerOptions));
-		mDrawerList.setOnItemClickListener(new DrawerItemClickListener(
-				drawerOptions, mDrawerLayout, this, mDrawerList));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
 
 		if (savedInstanceState != null) {
 
 		} else {
-			Fragment newFragment = new AllEventsFragment();
-			FragmentTransaction transaction = getFragmentManager()
-					.beginTransaction();
-			transaction.replace(R.id.content_frame, newFragment);
-			transaction.commit();
+			getFragmentManager().beginTransaction()
+					.replace(R.id.content_frame, new AllEventsFragment())
+					.commit();
 
 		}
-
-		// ActionBarDrawerToggle ties together the the proper interactions
-		// between the sliding drawer and the action bar app icon
-		// mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
-		// mDrawerLayout, /* DrawerLayout object */
-		// R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
-		// R.string.drawer_open, /* "open drawer" description for accessibility
-		// */
-		// R.string.drawer_close /* "close drawer" description for accessibility
-		// */
-		// ) {
-		// public void onDrawerClosed(View view) {
-		// getActionBar().setTitle(mTitle);
-		// invalidateOptionsMenu(); // creates call to
-		// // onPrepareOptionsMenu()
-		// }
-		//
-		// public void onDrawerOpened(View drawerView) {
-		// getActionBar().setTitle(mDrawerTitle);
-		// invalidateOptionsMenu(); // creates call to
-		// // onPrepareOptionsMenu()
-		// }
-		// };
-		// mDrawerLayout.setDrawerListener(mDrawerToggle);
-
 	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+
+		Fragment newFragment = null;
+
+		switch (position) {
+		case 0:
+			newFragment = new AllEventsFragment();
+			break;
+		case 1:
+			newFragment = new FriendEvents();
+			break;
+		case 2:
+			newFragment = new EditAddFriends();
+			break;
+		case 3:
+			Intent i = new Intent(getApplicationContext(), AppSettings.class);
+			startActivityForResult(i, SETTINGS_RESULT);
+			break;
+		default:
+			break;
+		}
+		if (position != 3) {
+			// Create new fragment and transaction
+			getFragmentManager().beginTransaction()
+					.replace(R.id.content_frame, newFragment).commit();
+		}
+		mDrawerLayout.closeDrawer(mDrawerList);
+		// THIS FUCKIN LINE can deactivate selection... kind of reseting the
+		// already selected choice
+		mDrawerList.setItemChecked(position, false);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			selectItem(position);
+		}
+	}
+
 }

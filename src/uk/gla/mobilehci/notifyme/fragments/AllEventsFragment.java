dcd471158c9416db9e0360a1bb71e651add3a3df
CoRafael <rconst04@cs.ucy.ac.cy>
2015-03-10 20:29:47 +0000

    STUPID GIT FIX

--- src/uk/gla/mobilehci/notifyme/fragments/AllEventsFragment.java
+++ src/uk/gla/mobilehci/notifyme/fragments/AllEventsFragment.java
@@ -2,24 +2,14 @@ package uk.gla.mobilehci.notifyme.fragments;
 
 import uk.gla.mobilehci.notifyme.R;
 import android.app.Fragment;
-import android.content.res.Resources;
-import android.location.Location;
 import android.os.Bundle;
-import android.provider.SyncStateContract.Constants;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.LatLng;
 
 public class AllEventsFragment extends Fragment {
 

96b718956c79aac08392c457629a0e226c430a0b
2156744b <2156744b@student.gla.ac.uk>
2015-03-10 23:44:57 +0000

    Changed appsetings

--- src/uk/gla/mobilehci/notifyme/AppSettings.java
+++ src/uk/gla/mobilehci/notifyme/AppSettings.java
@@ -21,19 +21,19 @@ public class AppSettings extends PreferenceActivity {
 		getFragmentManager().beginTransaction()
 				.replace(android.R.id.content, new MyPreferenceFragment())
 				.commit();
-		
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	    // Respond to the action bar's Up/Home button
-	    case android.R.id.home:
-	        NavUtils.navigateUpFromSameTask(this);
-	        return true;
-	    }
-	    return super.onOptionsItemSelected(item);
+		switch (item.getItemId()) {
+		// Respond to the action bar's Up/Home button
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	public static class MyPreferenceFragment extends PreferenceFragment {
